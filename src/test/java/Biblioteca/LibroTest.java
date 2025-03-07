b0c622c4ae95dcf1ac31728bb1bc80fd78ddcd1c
Lara <lara.serralvo988@comunidadunir.net>
2025-03-07 19:08:21 +0100

    UPDATE: Añadidos más test

--- src/test/java/Biblioteca/LibroTest.java
+++ src/test/java/Biblioteca/LibroTest.java
@@ -6,17 +6,24 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LibroTest {
+    private Libro lib;
 
     @BeforeEach
     void setUp() {
+        lib = new Libro("Marina", "Carlos Ruiz Zafon", 2000);
     }
 
     @Test
     void getTitulo() {
+        System.out.println("el título es 'Marina");
+        assertEquals("El Hobbit", lib.getTitulo(), "el título es 'Marina");
     }
 
     @Test
     void setTitulo() {
+        lib.setTitulo("Valeria");
+        System.out.println("El título debe ser Valeria");
+        assertEquals("Valeria", lib.getTitulo(), "El nuevo título tieen que ser Valeria");
     }
 
     @Test
